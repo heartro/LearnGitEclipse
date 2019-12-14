cc8ecc47133dd90eb957eac9eb46df8201d6505e
lisi <lisi@163.com>
2019-12-14 09:53:29 +0800

    李四修改了Lisi.java和ZhangSan.java这两个文件。

--- src/main/java/Lisi.java
+++ src/main/java/Lisi.java
@@ -2,5 +2,6 @@
 public class Lisi {
 	public static void main(String[] args) {
 		System.out.println("李四：大家好，我是后端开发程序员。");
+		System.out.println("李四：现在流行前后端分离开发啊，看来VUE或者React之类的技术必须有所了解。");
 	}
 }
--- src/main/java/ZhangSan.java
+++ src/main/java/ZhangSan.java
@@ -2,5 +2,6 @@
 public class ZhangSan {
 	public static void main(String[] args) {
 		System.out.println("张三：大家好，我是项目经理张三。");
+		System.out.println("李四To张三：老大好！");
 	}
 }
